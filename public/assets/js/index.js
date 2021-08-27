fab6055ea13200a5945fd286e1a5aaffb1fa2e06
Leonardo Abanto <leonardo_abanto@hotmail.com>
2021-08-27 16:46:57 -0500

    enabling CORS (release 2.0)

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,7 @@ $('a[href="#"]').click(function(event){
 
 function submitToAPI(e) {
     e.preventDefault();
-    var URL = "https://gyzcd1a7nl.execute-api.us-east-1.amazonaws.com/beta/contacto";
+    var URL = "https://gyzcd1a7nl.execute-api.us-east-1.amazonaws.com/prod/contacto";
 
          var Namere = /[A-Za-z]{1}[A-Za-z]/;
          if (!Namere.test($("#name-input").val())) {
@@ -38,7 +38,7 @@ function submitToAPI(e) {
 
     $.ajax({
       type: "POST",
-      url : "https://gyzcd1a7nl.execute-api.us-east-1.amazonaws.com/beta/contacto",
+      url : "https://gyzcd1a7nl.execute-api.us-east-1.amazonaws.com/prod/contacto",
       dataType: "json",
       crossDomain: "true",
       contentType: "application/json; charset=utf-8",
@@ -47,7 +47,7 @@ function submitToAPI(e) {
       
       success: function () {
         // clear form and show a success message
-        alert("Se envió el mensaje con éxito");
+        alert("Gracias. Su mensaje fue enviado con éxito");
         document.getElementById("contact-form").reset();
     location.reload();
       },
